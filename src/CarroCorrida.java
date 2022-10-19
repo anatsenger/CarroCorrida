0f8883b333f00717fa7a7f738f09256aaf5845a4
anatsenger <anathaissenger@hotmail.com>
2022-10-19 19:22:38 -0300

    Initial commit

---
+++ src/CarroCorrida.java
@@ -0,0 +1,60 @@
+public class CarroCorrida {
+
+    private Integer numeroCarro;
+    private float velocidadeMaxima;
+    private float velocidadeAtual;
+    private boolean ligado;
+    private Piloto piloto;
+
+    public CarroCorrida(Integer numeroCarro, Piloto piloto, float velocidadeMaxima, float velocidadeAtual, boolean ligado){
+        this.numeroCarro = numeroCarro;
+        this.piloto = piloto;
+        this.velocidadeAtual = velocidadeAtual;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.ligado = ligado;
+    }
+    public Integer getNumeroCarro(){return numeroCarro;}
+    public void setNumeroCarro(Integer numeroCarro){this.numeroCarro = numeroCarro;}
+
+    public float getVelocidadeMaxima(){return velocidadeMaxima;}
+    public void setVelocidadeMaxima(float velocidadeMaxima){this.velocidadeMaxima = velocidadeMaxima;}
+
+    public float getVelocidadeAtual(){return velocidadeAtual;}
+    public void setVelocidadeAtual(float velocidadeAtual){
+        if(velocidadeAtual < velocidadeMaxima){
+            this.velocidadeAtual = velocidadeAtual;
+        }
+    }
+
+    public boolean getLigado(){return ligado;}
+    public void setLigado(boolean ligado){this.ligado = ligado;}
+
+    public Piloto getPiloto(){return piloto;}
+    public void setPiloto(Piloto piloto){this.piloto = piloto;}
+
+    public static void acelera(float velocidade, CarroCorrida carro){
+        if(carro.ligado){
+            carro.setVelocidadeAtual(carro.getVelocidadeAtual() + velocidade);
+        }
+    }
+
+    public static void freia(float velocidade, CarroCorrida carro){
+        if(carro.ligado){
+            carro.setVelocidadeAtual(carro.getVelocidadeAtual() - velocidade);
+        }
+    }
+
+    public static void parar(CarroCorrida carro){
+            carro.setVelocidadeAtual(0);
+    }
+
+    public static void ligar(CarroCorrida carro, boolean ligado){
+        carro.setLigado(true);
+    }
+
+    public static void desligar(CarroCorrida carro, boolean ligado, float velocidadeAtual){
+        if(velocidadeAtual == 0){
+            carro.setLigado(false);
+        }
+    }
+}
---
+++ src/Piloto.java
@@ -0,0 +1,32 @@
+public class Piloto {
+    public static void main(String[] args) {
+        Piloto Jackson = new Piloto("Jackson", 24, Sexo.Masculino, "Skol");
+        CarroCorrida marea = new CarroCorrida(001, Jackson, 200, 0, true);
+
+    }
+    private String piloto;
+    private Integer idade;
+    private Sexo sexo;
+    private String equipe;
+
+    public Piloto(String piloto, Integer idade, Sexo sexo, String equipe){
+        this.piloto = piloto;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.equipe = equipe;
+    }
+    public String getPiloto(){return piloto;}
+    public void setPiloto(String piloto){this.piloto = piloto;}
+
+    public Integer getIdade(){return idade;}
+    public void setIdade(Integer idade){ this.idade = idade;}
+
+    public Sexo getSexo(){return sexo;}
+    public void setSexo(Sexo sexo){this.sexo = sexo;}
+
+    public String getEquipe(){return equipe;}
+    public void setEquipe(String equipe){this.equipe = equipe;}
+
+
+
+}
